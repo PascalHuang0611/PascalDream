b2e91a8836824b58f9271399d10c8a1e07bc5af3
PascalHuang0611 <a4568245682@gmail.com>
2025-09-23 14:21:18 +0800

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -8,139 +8,168 @@ const MONSTERS_DATABASE = [
     maxHp: 80,
     atk: 10,
     def: 3,
-    spd: 10
+    spd: 10,
+    isTestMonster: false
   },
   {
     name: "哥布林",
     maxHp: 120,
     atk: 15,
     def: 5,
-    spd: 12
+    spd: 12,
+    isTestMonster: false
   },
   {
     name: "兇猛野狼",
     maxHp: 100,
     atk: 20,
     def: 4,
-    spd: 25
+    spd: 25,
+    isTestMonster: false
   },
   {
     name: "石頭人",
     maxHp: 250,
     atk: 8,
     def: 20,
-    spd: 5
+    spd: 5,
+    isTestMonster: false
   },
   {
     name: "鷹身女妖",
     maxHp: 150,
     atk: 18,
     def: 8,
-    spd: 30
+    spd: 30,
+    isTestMonster: false
   },
   {
     name: "食人魔",
     maxHp: 300,
     atk: 25,
     def: 15,
-    spd: 8
+    spd: 8,
+    isTestMonster: false
   },
   {
     name: "劇毒蜘蛛",
     maxHp: 180,
-    atk: 35,
+    atk: 22,
     def: 10,
-    spd: 22
+    spd: 20,
+    isTestMonster: false
   },
   {
-    name: "沼澤蜥蜴人",
-    maxHp: 350,
-    atk: 30,
-    def: 25,
-    spd: 15
-  },
-  {
-    name: "地獄犬",
-    maxHp: 280,
-    atk: 45,
-    def: 20,
-    spd: 35
-  },
-  {
-    name: "獨眼巨人",
-    maxHp: 500,
-    atk: 40,
+    name: "裝甲野豬",
+    maxHp: 400,
+    atk: 20,
     def: 30,
-    spd: 10
+    spd: 10,
+    isTestMonster: false
   },
   {
-    name: "蛇髮女妖",
-    maxHp: 380,
-    atk: 50,
-    def: 28,
-    spd: 32
+    name: "元素精靈",
+    maxHp: 120,
+    atk: 35,
+    def: 5,
+    spd: 35,
+    isTestMonster: false
   },
   {
     name: "牛頭怪",
-    maxHp: 600,
-    atk: 55,
-    def: 40,
-    spd: 18
-  },
-  {
-    name: "冰霜元素",
-    maxHp: 450,
-    atk: 65,
-    def: 35,
-    spd: 28
-  },
-  {
-    name: "火焰巨靈",
-    maxHp: 480,
-    atk: 70,
-    def: 32,
-    spd: 38
-  },
-  {
-    name: "奇美拉",
-    maxHp: 750,
-    atk: 60,
-    def: 50,
-    spd: 25
+    maxHp: 500,
+    atk: 40,
+    def: 25,
+    spd: 15,
+    isTestMonster: false
   },
   {
     name: "獅鷲",
-    maxHp: 650,
-    atk: 75,
-    def: 45,
-    spd: 50
+    maxHp: 350,
+    atk: 45,
+    def: 20,
+    spd: 40,
+    isTestMonster: false
   },
   {
-    name: "巫妖",
-    maxHp: 550,
+    name: "獨眼巨人",
+    maxHp: 800,
+    atk: 55,
+    def: 35,
+    spd: 12,
+    isTestMonster: false
+  },
+  {
+    name: "奇美拉",
+    maxHp: 600,
+    atk: 60,
+    def: 30,
+    spd: 28,
+    isTestMonster: false
+  },
+  {
+    name: "深海巨魷",
+    maxHp: 750,
+    atk: 50,
+    def: 40,
+    spd: 22,
+    isTestMonster: false
+  },
+  {
+    name: "熔岩巨像",
+    maxHp: 1200,
+    atk: 70,
+    def: 60,
+    spd: 8,
+    isTestMonster: false
+  },
+  {
+    name: "霜巨人",
+    maxHp: 1000,
+    atk: 80,
+    def: 50,
+    spd: 18,
+    isTestMonster: false
+  },
+  {
+    name: "雷鳥",
+    maxHp: 700,
     atk: 90,
     def: 40,
-    spd: 30
+    spd: 50,
+    isTestMonster: false
   },
   {
-    name: "深淵眼魔",
-    maxHp: 800,
-    atk: 85,
-    def: 60,
-    spd: 20
-  },
-  {
-    name: "青年龍",
-    maxHp: 1000,
-    atk: 100,
-    def: 70,
-    spd: 40
-  },
-  {
-    name: "上古巨龍",
+    name: "九頭蛇",
     maxHp: 1500,
+    atk: 100,
+    def: 65,
+    spd: 25,
+    isTestMonster: false
+  },
+  {
+    name: "黑曜石像鬼",
+    maxHp: 1100,
+    atk: 85,
+    def: 120,
+    spd: 30,
+    isTestMonster: false
+  },
+  {
+    name: "遠古紅龍",
+    maxHp: 2000,
     atk: 120,
     def: 80,
-    spd: 35
+    spd: 45,
+    isTestMonster: false
+  },
+  {
+    name: "測試用",
+    maxHp: 999999,
+    atk: 99999,
+    def: 99999,
+    spd: 1000,
+    isTestMonster: true
   }
 ];
+
